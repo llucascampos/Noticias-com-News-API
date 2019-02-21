4b276799b85010c6bed64260eb378447fe902775
Lucas Lima <lucascampos07@hotmail.com>
2019-02-20 23:38:41 -0300

    Primeiro commit

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+<div>
     <v-toolbar  class="py-4" >
-     <div style="width: 100%;text-align: center;">
-      <img src="../assets/logomultiedro.png">
-    </div>
+      <div style="width: 100%;text-align: center;">
+        <img src="../assets/logomultiedro.png">
+      </div>
     </v-toolbar>
 
     <div class="big-header">
@@ -17,18 +17,17 @@
           <v-flex ></v-flex>
           <v-flex sm12  md5 class="pt-5">
               <v-text-field
-                label="Digite o nome da empresa"
+                label="Digite Sua Pesquisa"
                 v-model="pesquisa"
                 color="white"
                 class="input-texto"
                 dark
-                @keyup.enter="chamada(), scroll()"
+                @keyup.enter="chamandoDados(), ativandoScroll()"
               ></v-text-field>
               <div style="text-align:right;">
                 <v-btn
-                @click="chamada(), scroll() "
+                @click="chamandoDados(), ativandoScroll()"
                 color="#7b2ede" dark style="margin-top: 0px !important;"
-                :disabled="verificaCampo"
                 >Pesquisar</v-btn>
               </div>
           </v-flex>
@@ -36,7 +35,10 @@
       </v-container>
     </div>
 
-      <v-layout justify-center wrap v-if="termoPesquisado">
+<v-container fluid>
+
+  <!-- Mostrando termo pesquisado -->
+     <v-layout justify-center wrap v-if="termoPesquisado" class="mb-4">
         <v-flex xs12>
           <h2 class="termo-titulo">Termo Pesquisado:</h2>
         </v-flex>
@@ -45,7 +47,8 @@
         </v-flex>
       </v-layout>
 
-      <v-layout justify-center class="my-5" v-if="loadingItem">
+      <!-- Mostrando carregamento -->
+      <v-layout justify-center class="my-5" v-if="carregando">
           <v-progress-circular
           indeterminate
           color="purple"
@@ -54,108 +57,105 @@
         </v-progress-circular>
       </v-layout>
 
-<v-container fluid>
-      <v-layout wrap row >
+    <!-- Conteudo -->
 
-        <!-- Cards de Instagram -->
-        <v-flex sm12 md4 class="pa-3" v-if="instagram.edges != null">
-          <v-card class="px-2">
-            <v-card-title><img src="../assets/instagram.png"><h3 class="ml-3">Instagram</h3></v-card-title>
+    <v-layout row wrap>
+
+  <!-- Card Twitter -->
+      <v-flex sm12 md6  class="pa-3 text-md-center"  v-if="scoreTwitter != null">
+        <v-card class="px-2">
+            <v-card-title><img src="../assets/twitter.png" style="height:25px"><h3 class="ml-3">Twitter</h3></v-card-title>
             <v-divider class="py-2"></v-divider>
             <div class="cardSocial">
-              <div v-if="instagram.edges == '' || instagram.edges == null "  style="text-align: center">
-                <h3>Dados Não Encontrado</h3>
-              </div>
-              <div v-else v-for="post in instagram.edges" class="divisor" >
 
-                <!-- Aqui o Conteúdo -->
-                <img :src="post.node.display_url" style="width: 100%; border-radius: 10px;">
-                <div v-for="conteudoPost in post.node.edge_media_to_caption.edges">
-                  {{ conteudoPost.node.text }}
+              <div v-if="dadosTwitter == ''" style="text-align: center">
+                <h3>Dados Não Encontrados</h3>
+              </div>
+
+              <div v-else v-for="tweet in dadosTwitter" :key="tweet.id"  style="width: 100%" class="text-xs-center">
+                <tweet :id="tweet.id_str"  style="margin-left: 7%"></tweet>
+
+                <h4> {{formatandoScore(tweet.score)}} </h4>
+                <img v-if="tweet.score < -0.5" :src="score1" style="width: 65%" class="mb-4">
+                <img v-if="tweet.score >= -0.5 && tweet.score < -0.1" :src="score2" style="width: 65%" class="mb-4">
+                <img v-if="tweet.score >= -0.1 && tweet.score <= 0.1" :src="score3" style="width: 65%" class="mb-4">
+                <img v-if="tweet.score > 0.1   && tweet.score <= 0.5" :src="score4" style="width: 65%" class="mb-4">
+                <img v-if="tweet.score > 0.5" :src="score5" style="width: 65%" class="mb-4"> 
+              </div>
+
+          </div>
+        </v-card>
+      </v-flex>
+
+       <!-- Cards de Notícias -->
+        <v-flex  sm12 md6 class="pa-3" v-if="scoreNoticia != null">
+          <v-card class="px-2">
+            <v-card-title><v-icon>language</v-icon><h3 class="ml-3">News</h3></v-card-title>
+            <v-divider class="py-2"></v-divider>
+
+            <div class="cardSocial">
+              <div v-if="dadosNoticia == ''"  style="text-align: center">
+                <h3>Dados Não Encontrados</h3>
+              </div>
+
+              <div v-if="scoreNoticia != null" class="py-2 px-4">
+                <div v-for="noticia in dadosNoticia" :key="noticia.title" class="divisor text-xs-center mb-3">
+                  <h2 class="pa-2">{{noticia.title }} </h2>
+                  <a :href="noticia.url"><img :src="noticia.urlToImage" style="width:95%"></a><br><br>
+                  <p class="px-4" style="font-size: 17px"> {{ noticia.description }}</p> <br>
+
+                  <h4> {{formatandoScore(noticia.score)}}</h4>
+                  <img v-if="noticia.score < -0.5" :src="score1" style="width: 70%">
+                  <img v-if="noticia.score >= -0.5 && noticia.score < -0.1" :src="score2" style="width: 70%">
+                  <img v-if="noticia.score >= -0.1 && noticia.score <= 0.1" :src="score3" style="width: 70%">
+                  <img v-if="noticia.score > 0.1   && noticia.score <= 0.5" :src="score4" style="width: 70%">
+                  <img v-if="noticia.score > 0.5" :src="score5" style="width: 70%">
                 </div>
-                <v-divider class="py-1"></v-divider>
               </div>
+
             </div>
           </v-card>
         </v-flex>
 
-         <!-- Cards de Twitters -->
-        <v-flex sm12 md4  class="pa-3"  v-if="twitters != null">
-          <v-card class="px-2">
-             <v-card-title><img src="../assets/twitter.png"><h3 class="ml-3">Twitter</h3></v-card-title>
-            <v-divider class="py-2"></v-divider>
-            <div class="cardSocial">
-              <div v-if="twitters == ''" style="text-align: center">
-                <h3>Dados Não Encontrado</h3>
-              </div>
-              <div v-else v-for="tweet in twitters" :key="tweet.id">
-                <tweet :id="tweet.id_str"></tweet>
-              </div>
-            </div>
-          </v-card>
-        </v-flex>
+    </v-layout>
+  </v-container>
 
-        <!-- Cards de Notícias -->
-        <v-flex  sm12 md4 class="pa-3"  v-if="noticias != null" >
-          <v-card class="px-2">
-             <v-card-title><v-icon>language</v-icon><h3 class="ml-3">News</h3></v-card-title>
-            <v-divider class="py-2"></v-divider>
-            <div class="cardSocial">
-              <div v-if="noticias == ''"  style="text-align: center">
-                <h3>Dados Não Encontrado</h3>
-              </div>
-              <div  v-for="noticia in noticias" class="divisor" >
-                <h3>{{noticia.title }} </h3>
-                <img :src="noticia.urlToImage" style="width:100%">
-                <p> {{ noticia.description }}</p>
-                <p> {{ noticia.source.name }}</p>
-                <v-divider class="py-1"></v-divider>
-              </div>
-
-
-
-            </div>
-          </v-card>
-        </v-flex>
-      </v-layout>
-      <v-btn @click="enviandoTextos()"> enviar</v-btn>
-
-
-    {{resultado}}
-
-</v-container>
-    <v-footer dark height="auto" style=" text-align: center">
-      <div style="width: 100%;text-align: center;">
+   <v-footer dark height="50" class="grey darken-3 justify-center">
         &copy;2019 — <strong>Multiedro</strong>
-      </div>
-    </v-footer>
-
-  </div>
+  </v-footer>
+</div>
 </template>
 
 <script>
-import * as easings from 'vuetify/es5/util/easing-patterns'
-import axios from 'axios'
+import axios from "axios";
 import { Tweet, Moment, Timeline } from 'vue-tweet-embed'
+import * as easings  from 'vuetify/es5/util/easing-patterns'
+
+import imgMtTriste   from '../assets/nivelMtTriste.png'
+import imgMeioTriste from '../assets/nivelMeioTriste.png'
+import imgNeutro     from '../assets/nivelNeutro.png'
+import imgMeioFeliz  from '../assets/nivelMeioFeliz.png'
+import imgMtFeliz    from '../assets/nivelMtFeliz.png'
+
 export default {
-  components:{
-    Tweet,
+  components: {
+    Tweet
   },
 
-  data(){
-    return{
-      pesquisa: '',
-      titulosNoticia: [],
-      instagram: [],
-      textoIntagram: [],
-      textos: [],
-      twitters: null,
-      noticias: null,
-      loadingItem: false,
-      erro: {
-        loaded: false,
-        msgError: 'Conteúdo não carregado.'
-      },
+  data() {
+    return {
+      pesquisa: null,
+      termoPesquisado: null,
+      carregando: false,
+      dadosInstagram: null,
+      dadosTwitter: null,
+      dadosNoticia: null,
+      scoreInstagram: null,
+      scoreNoticia: null,
+      scoreTwitter: null,
+      imgInstagram: null,
+      termoNaoEncontrado: false,
+
       type: 'number',
       number: 500,
       duration: 400,
@@ -163,88 +163,89 @@ export default {
       easing: 'linear',
       easings: Object.keys(easings),
       termoPesquisado: '',
-      resultado: []
-    }
+      score1: imgMtTriste,  
+      score2: imgMeioTriste,
+      score3: imgNeutro,
+      score4: imgMeioFeliz,
+      score5: imgMtFeliz,
+
+    };
   },
 
-  methods:{
+  methods: {
 
-    // pegandoTexto(texto, tipo){
-    //   if(tipo == "noticias"){
-    //     if(texto != null || texto != undefined || texto != ''){
-    //       if(this.titulosNoticia.length<= 10){
-    //         this.tituloNoticia.push(texto)
-    //       }
-    //     }
-    //   return texto
-    //   }
-
-    //   if(tipo == "instagram"){
-    //     if(texto != null || texto != undefined || texto != ''){
-    //       if(this.textoIntagram.length<= 10){
-    //         this.textoIntagram.push(texto)
-    //       }
-    //     }
-    //   return texto
-    //   }
-    // },
-
-    enviandoTextos(){
-      axios.all([
-        axios.post(`https://backend-back-dot-multiedro-summit.appspot.com/instanlp`, this.textoIntagram),
-        axios.post(`https://backend-back-dot-multiedro-summit.appspot.com/newsnlp`, this.titulosNoticia),
-        ]).then(axios.spread((instaRes, resTitulo) => {
-          this.resultado = resTitulo.data
-
-          })).catch(function (error) {
-          console.log(error.response.status);
-        })
-    },
-
-    scroll(){
+    ativandoScroll(){
       let vm = this
       setTimeout(function () {
                vm.$vuetify.goTo(vm.target, vm.options)
             }, 10);
-
     },
 
-    chamada(){
-      let vm = this;
-      this.loadingItem = true;
-      this.erro.loaded = false;
-      this.instagram = [];
-      this.noticias = null
-      this.twitters = null
+    async juntandoObjetos(objeto1, objeto2){
 
-     // if(this.pesquisa.indexOf(' ') != -1) this.pesquisa = this.pesquisa.split(' ').join('');
+        for (let i = 0; i<=objeto1.length; i++){
+          Object.defineProperty(objeto1[i], 'score', {
+            enumerable: true,
+            writable: true,
+            value: objeto2[i].score
+        })
+        }
+    },
+
+    formatandoScore(texto){
+      return texto.toFixed(2)     
+    },
+
+    chamandoDados() {
+      this.carregando = true;
+      this.dadosInstagram = null;
+      this.dadosTwitter = null;
+      this.dadosNoticia = null;
+      this.scoreInstagram = null;
+      this.scoreNoticia = null;
+      this.scoreTwitter = null;
+      this.imgInstagram = null;
       this.termoPesquisado = this.pesquisa
 
       axios.all([
-        axios.get(`https://www.instagram.com/explore/tags/${this.pesquisa}/?__a=1&language=pt`),
         axios.get(`https://newsapi.org/v2/everything?q=${this.pesquisa}&apiKey=e787a0831432407f9925ce02d22703a3&language=pt`),
-        axios.get(`https://backend-dot-multiedro-summit.appspot.com/tweets?q=${this.pesquisa}&lang=pt&-filter:links`)
-        ]).then(axios.spread((instaRes, newsRes, twitterRes) => {
-          this.instagram = instaRes.data.graphql.hashtag.edge_hashtag_to_media
-          this.texto = instaRes.data.graphql.hashtag.edge_hashtag_to_media.edges
-          this.noticias = newsRes.data.articles
-          this.titulosNoticia = this.noticias.map(noticia => noticia.title)
+        axios.get(`https://backend-dot-multiedro-summit.appspot.com/tweets?q=${this.pesquisa}&lang=pt&-filter:links`),
+        ]).then(axios.spread((responseNoticias, responseTwitter) => {
 
-          this.twitters = twitterRes.data.statuses
-          this.loadingItem = false;
-          this.scroll()
+          this.dadosTwitter = responseTwitter.data.statuses;
+          this.dadosNoticia = responseNoticias.data.articles
+
+        // Pegando os texto para enviar ao back
+          let apenasTitulosNoticia = this.dadosNoticia.map(noticia => noticia.title)
+          let apenasTextoTwitter = this.dadosTwitter.map(twitter => twitter.full_text)
+
+       //Mandando textos para avaliar o sentimeto na api google
+        axios.all([
+          axios.post(`https://backend-dot-multiedro-nlp.appspot.com/newsnlp`, apenasTitulosNoticia),
+          axios.post(`https://backend-dot-multiedro-nlp.appspot.com/newsnlp`, apenasTextoTwitter),
+        ]).then(axios.spread(( responseScoreNoticia, responseScoreTwitter) => {
+          this.scoreTwitter = responseScoreTwitter.data
+          this.scoreNoticia = responseScoreNoticia.data
+
+          this.juntandoObjetos(this.dadosNoticia, this.scoreNoticia)
+          this.juntandoObjetos(this.dadosTwitter, this.scoreTwitter)
+
+          this.pesquisa = null
+          this.carregando = false;
+          this.ativandoScroll()
+          })) // Fechando o .then
 
           })).catch(function (error) {
-          vm.erro.loaded = true;
-          vm.loadingItem = false;
-          if(error.response.status == 404) vm.erro.msgError = 'Dado não encontrado, digite novamente sua pesquisa.'
-          else vm.erro.msgError = error.response.data
+
           console.log(error.response.status);
+          console.log(error);
         })
     },
+
   },
 
-   computed: {
+  computed: {
+
       target () {
         const value = this[this.type]
          return value
@@ -257,12 +258,8 @@ export default {
           easing: this.easing
         }
       },
-      verificaCampo() {
-        if(this.pesquisa == null || this.pesquisa == '') return true;
-        else return false;
-      }
-   }
-}
+  }
+};
 </script>
 
 <style scoped>
@@ -323,6 +320,7 @@ h1.titulo-page {
     font-size: 25px;
     letter-spacing: -1px;
     width: auto;
+
 }
 
 .termo-titulo, .termo-pesquisado {
